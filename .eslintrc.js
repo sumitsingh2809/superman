00ae2c70af00fcd0b13fe605cc14935e72a5dca6
Sumit Singh <sumitsingh@chingari.io>
2022-07-28 21:05:00 +0530

    chore: lint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ['airbnb-base'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        indent: [0, 'tab'],
+        'no-tabs': 0,
+        'comma-dangle': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+              js: 'never',
+              jsx: 'never',
+              ts: 'never',
+              tsx: 'never'
+            }
+        ]
+    },
+    settings: {
+        'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx']
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
+    }
+};
